247672a8abca0bff4cf150b5293c80f5659c9072
aeonyxio <aeonyxio@gmail.com>
2022-10-14 21:14:20 +0100

    fix file path

--- src/app.ts
+++ src/app.ts
@@ -21,25 +21,20 @@ export class Application {
     injector.get(PostController).init();
     injector.get(DocController).init();
 
-    const schemas = (
-      await Promise.all(
-        (
-          await arrayFromAsyncIterable(Deno.readDir("schema"))
-        ).map((file) => Deno.readTextFile(join("schema", file.name))),
-      )
-    ).map((i) => JSON.parse(i));
-
-    registerSchemaValidator(this.app, schemas);
+    registerSchemaValidator(this.app, [
+      JSON.parse(await Deno.readTextFile(join("schema", "api.json"))),
+      JSON.parse(await Deno.readTextFile(join("schema", "objects.json"))),
+    ]);
 
     this.app.use(router.routes());
     this.app.use(router.allowedMethods());
-    // this.app.addEventListener("listen", ({ hostname, port, secure }) => {
-    //   console.log(
-    //     `Listening on: ${secure ? "https://" : "http://"}${
-    //       hostname ?? "localhost"
-    //     }:${port}`
-    //   );
-    // });
+    this.app.addEventListener("listen", ({ hostname, port, secure }) => {
+      console.log(
+        `Listening on: ${secure ? "https://" : "http://"}${
+          hostname ?? "localhost"
+        }:${port}`
+      );
+    });
   }
 
   async start() {
