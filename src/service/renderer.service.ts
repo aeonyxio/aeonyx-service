ec4c0f94f6baf7328abc50ced82e8f93afd54907
aeonyxio <aeonyxio@gmail.com>
2022-10-28 21:33:44 +0100

    json formatting

--- src/service/renderer.service.ts
+++ src/service/renderer.service.ts
@@ -1,4 +1,4 @@
-import { render } from "https://deno.land/x/aeonyx_md@v0.0.3/mod.ts";
+import { render } from "https://deno.land/x/aeonyx_md@v0.0.4/mod.ts";
 
 export class RendererService {
   render = render;
